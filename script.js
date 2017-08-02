c6dedff435d2b3fee49e3b834756d0eede6530ac
sinshi kudo <sinshiwho@yahoo.com>
2017-08-02 12:32:00 +0200

    Change the core code

--- script.js
+++ script.js
@@ -7,15 +7,62 @@ setInterval(function() {
   function dec2bin(dec) {
     return dec.toString(2);
   };
-  var sec = dec2bin(6).toString().split("");
-  for (var i = sec.length - 1; i >= 0; i--) {
-    if (sec[i] == 1)
-      light(i+1);
+
+
+  if (s < 10) {
+    timelight(s);
+    console.log(s);
   }
-  console.log(sec);
+  else if (s > 10) {
+    var sec = s.toString().split("");
+    var Fsec = parseInt(sec[0]);
+    var Ssec = parseInt(sec[1]);
+    timelight(Fsec);
+    timelight(Ssec);
+    console.log(s);
+  }
 }, 1000)
 
 
 function light(x) {
   console.log(x + " is lighted");
+}
+
+function timelight(v) {
+  switch (v) {
+    case 1:
+      light(1);
+      break;
+    case 2:
+      light(2);
+      break;
+
+    case 3:
+      light(1);
+      light(2);
+      break;
+    case 4:
+      light(3);
+      break;
+    case 5:
+      light(1);
+      light(3);
+      break;
+    case 6:
+      light(2);
+      light(4);
+      break;
+    case 7:
+      light(1);
+      light(2);
+      light(3);
+      break;
+    case 8:
+      light(4);
+      break;
+    case 9:
+      light(1);
+      light(4);
+      break;
+  }
 }
