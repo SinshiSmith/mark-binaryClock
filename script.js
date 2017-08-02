b584514056e616dbfdd3716f260e67fc43af038f
sinshi kudo <sinshiwho@yahoo.com>
2017-08-02 16:59:20 +0200

    Working?!

--- script.js
+++ script.js
@@ -1,68 +1,90 @@
+
 setInterval(function() {
+  $("*").css("background-color","white");
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
+  timechecker(s, "seconds")
+  timechecker(m, "minutes")
+  timechecker(h, "hours")
 
-  function dec2bin(dec) {
-    return dec.toString(2);
-  };
-
-
-  if (s < 10) {
-    timelight(s);
-    console.log(s);
-  }
-  else if (s > 10) {
-    var sec = s.toString().split("");
-    var Fsec = parseInt(sec[0]);
-    var Ssec = parseInt(sec[1]);
-    timelight(Fsec);
-    timelight(Ssec);
-    console.log(s);
-  }
 }, 1000)
 
-
-function light(x) {
-  console.log(x + " is lighted");
+function timechecker(y, type) {
+  if (y < 10) {
+    timelight(y, type + "1");
+  } else if (y > 10) {
+    var sec = y.toString().split("");
+    var Fsec = parseInt(sec[0]);
+    var Ssec = parseInt(sec[1]);
+    timelight(Fsec, type + "2");
+    timelight(Ssec, type + "1");
+  }
 }
 
-function timelight(v) {
+function timelight(v, type) {
+
   switch (v) {
     case 1:
-      light(1);
+      light(1, type);
       break;
     case 2:
-      light(2);
+      light(2, type);
       break;
 
     case 3:
-      light(1);
-      light(2);
+      light(1, type);
+      light(2, type);
       break;
     case 4:
-      light(3);
+      light(4, type);
       break;
     case 5:
-      light(1);
-      light(3);
+      light(1, type);
+      light(4, type);
       break;
     case 6:
-      light(2);
-      light(4);
+      light(2, type);
+      light(4, type);
       break;
     case 7:
-      light(1);
-      light(2);
-      light(3);
+      light(1, type);
+      light(2, type);
+      light(4, type);
       break;
     case 8:
-      light(4);
+      light(8, type);
       break;
     case 9:
-      light(1);
-      light(4);
+      light(1, type);
+      light(8, type);
       break;
+  }
+}
+
+function light(x, kind) {
+  var  k = x.toString();
+  switch (kind) {
+    case "seconds1":
+        $("#s1" + x).css("background-color","red");
+      break;
+    case "seconds2":
+      $("#s2" + x).css("background-color","red");
+      break;
+    case "minutes1":
+      $("#m1" +x+"").css("background-color","red");
+      break;
+    case "minutes2":
+      $("#m2"+x+"").css("background-color","red");
+      break;
+    case "hours1":
+      $("#h1" + x+"").css("background-color","red");
+      break;
+    case "hours2":
+      $("#h2" + x+"").css("background-color","red");
+      break;
+    default:
+
   }
 }
